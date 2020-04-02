6226a3f5aabdf49ba728f50558dd48853e92551d
Tom Rix <trix@redhat.com>
2020-04-02 08:16:57 -0700

    Fix some checkpatch issues
    Signed-off-by: Tom Rix <trix@redhat.com>

--- jitterz.c
+++ jitterz.c
@@ -1,15 +1,15 @@
+// SPDX-License-Identifier: GPL-2.0-only
 /*
  * jitterz
  *
- * Copyright 2019 Tom Rix <trix@redhat.com>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License Version
- * 2 as published by the Free Software Foundation.
+ * Copyright 2019-2020 Tom Rix <trix@redhat.com>
  *
  */
 
+
+#ifndef _GNU_SOURCE
 #define _GNU_SOURCE
+#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
@@ -29,27 +29,35 @@
 #include <sys/stat.h>
 #include <math.h>
 
-#define VERSION 0.9
-
-static int cpu = 0;
-static int clocksel = 0;
+static int cpu;
+static int clocksel;
 static int policy = SCHED_FIFO;
 static int priority = 5;
 
-static inline uint64_t tsc()
+#define CHECK_LOST_TIME()					\
+	do {							\
+		if (d >= dt_min) {				\
+			lt += d;				\
+			for (j = 16; j > 0; j--) {		\
+				if (d >= b[j - 1].s) {		\
+					b[j - 1].c =		\
+						b[j - 1].c + 1;	\
+					break;			\
+				}				\
+			}					\
+		}						\
+	} while (0)						\
+
+static inline uint64_t tsc(void)
 {
 	uint64_t ret = 0;
 	uint32_t l, h;
+
 	__asm__ __volatile__("lfence");
 	__asm__ __volatile__("rdtsc" : "=a"(l), "=d"(h));
 	ret = ((uint64_t)h << 32) | l;
 	return ret;
 }
-
-struct bucket {
-	uint64_t s;
-	uint64_t c;
-};
 
 static int move_to_core(int core_i)
 {
@@ -60,7 +68,7 @@ static int move_to_core(int core_i)
 	return sched_setaffinity(0, sizeof(cpus), &cpus);
 }
 
-static int set_sched()
+static int set_sched(void)
 {
 	struct sched_param p = { 0 };
 
@@ -73,24 +81,32 @@ static long read_cpuinfo_cur_freq(int core_i)
 	uint64_t fs = -1;
 	char path[80];
 	struct stat sb;
+	int i;
+	char *freq[2] = {
+		"cpuinfo_cur_freq",
+		/* assumes a busy wait will be run at the max freq */
+		"cpuinfo_max_freq",
+	};
+	for (i = 0; i < 2; i++) {
+		snprintf(path, 80,
+			 "/sys/devices/system/cpu/cpu%d/cpufreq/%s",
+			 core_i, freq[1]);
+		if (!stat(path, &sb)) {
+			FILE *f = 0;
 
-	snprintf(path, 80,
-		 "/sys/devices/system/cpu/cpu%d/cpufreq/cpuinfo_cur_freq",
-		 core_i);
-	if (!stat(path, &sb)) {
-		FILE *f = 0;
-		f = fopen(path, "rt");
-		if (f) {
-			fscanf(f, "%lu", &fs);
-			fclose(f);
+			f = fopen(path, "rt");
+			if (f) {
+				fscanf(f, "%lu", &fs);
+				fclose(f);
+			} else {
+				perror(path);
+			}
 		} else {
 			perror(path);
 		}
-	} else {
-		perror(path);
 	}
 
-	if (fs == (uint64_t) -1 ) {
+	if (fs == (uint64_t) -1) {
 		printf("Error reading CPU frequency for core %d\n", core_i);
 		exit(1);
 	}
@@ -108,7 +124,7 @@ static void display_help(int error)
 	       "                           0 = CLOCK_MONOTONIC (default)\n"
 	       "                           1 = CLOCK_REALTIME\n"
 	       "-p PRIO  --priority=PRIO   priority of highest prio thread\n"
-	       "	 --policy=NAME     policy of measurement thread, where NAME may be one\n"
+	       "         --policy=NAME     policy of measurement thread, where NAME may be one\n"
 	       "                           of: other, normal, batch, idle, fifo or rr.\n");
 	if (error)
 		exit(EXIT_FAILURE);
@@ -172,7 +188,7 @@ static void process_options(int argc, char *argv[], int max_cpus)
 		 * Options for getopt
 		 * Ordered alphabetically by single letter name
 		 */
-		static struct option long_options[] = {
+		static const struct option long_options[] = {
 			{ "clock", required_argument, NULL, OPT_CLOCK },
 			{ "cpu", required_argument, NULL, OPT_CPU },
 			{ "priority", required_argument, NULL, OPT_PRIORITY },
@@ -215,9 +231,12 @@ int main(int argc, char **argv)
 	struct timespec tvs, tve;
 	double sec;
 	uint64_t fs, fe, fr;
-	unsigned i, j, rt = 60;
+	unsigned int i, j, rt = 60;
 	uint64_t dt = 1500;
-	struct bucket b[16];
+	struct bucket {
+		uint64_t s;
+		uint64_t c;
+	} b[16];
 	uint64_t frs, fre, lt;
 
 	process_options(argc, argv, max_cpus);
@@ -236,18 +255,16 @@ int main(int argc, char **argv)
 	fr = fs = 0;
 	fe = 1;
 	while (fs != fe) {
-	retry:
+retry:
 		if (!fr) {
 			fs = read_cpuinfo_cur_freq(cpu);
 			fe = 0;
 		} else {
 			fs = fr;
 		}
-		// printf("%lu\n", fs);
 		uint64_t dt_min = (dt * fs) / 1000000;
-		lt = 0;
-		// printf("%lu\n", dt_min);
 
+		lt = 0;
 		for (j = 0; j < 16; j++) {
 			b[j].c = 0;
 			if (j == 0)
@@ -266,27 +283,19 @@ int main(int argc, char **argv)
 			s = tsc();
 			e = s;
 			e += fs;
-			if (e < s) {
+			if (e < s)
 				goto retry;
-			}
 			so = s;
 
 			while (1) {
+				uint64_t d;
+
 				s = tsc();
-				if (s > so) {
-					uint64_t d = s - so;
-					if (d >= dt_min) {
-						lt += d;
-						for (j = 16; j > 0; j--) {
-							if (d >= b[j - 1].s) {
-								b[j - 1].c =
-									b[j - 1].c +
-									1;
-								break;
-							}
-						}
-					}
-				}
+				if (s == so)
+					continue;
+
+				d = s - so;
+				CHECK_LOST_TIME();
 				if (s >= e)
 					break;
 				so = s;
@@ -297,27 +306,19 @@ int main(int argc, char **argv)
 		sec = tve.tv_sec - tvs.tv_sec +
 		      (tve.tv_nsec - tvs.tv_nsec) / 1e9;
 		if ((fabs(sec - rt) / (double)rt) > 0.01) {
-			//printf("%f %u\n", sec, rt);
 			if (fre > frs) {
 				fr = (fre - frs) / (1000 * sec);
 				fe = fr * 1000;
-				//printf("%lu\n", fr);
 			}
 			goto retry;
 		}
-
-		// printf("%f %f\n", sec, 1/sec);
 		if (!fr) {
 			fe = read_cpuinfo_cur_freq(cpu);
 			fe *= 1000;
 		}
 	}
-
-	// printf("%lu\n", fs / 1000);
-	for (j = 0; j < 16; j++) {
-		// printf("%lu %lu\n", b[j].s, b[j].c);
+	for (j = 0; j < 16; j++)
 		printf("%lu\n", b[j].c);
-	}
 
 	if (lt != fs) {
 		printf("Lost time %f\n", (double)lt / (double)fs);
